f62234a5303ed8fdf09fa8a8dda89a0474b5d7ad
Nick Pilon <npilon@lexmachina.com>
2021-04-27 15:14:20 -0700

    Sketch work queue API

--- src/hank/work_queue.py
+++ src/hank/work_queue.py
@@ -1,16 +1,37 @@
 """Work queues can submit and listen for tasks."""
 
+from collections.abc import Generator
 from typing import Protocol
+
+from .task import Task
 
 
 class WorkQueue(Protocol):
-    pass
+    def send(self, task: Task):
+        pass
+
+    def receive(self) -> Generator[Task]:
+        pass
 
 
 class LocalMemoryWorkQueue:
-    pass
+    def __init__(self):
+        self.queue = []
+
+    def send(self, task: Task):
+        self.queue.append(task)
+
+    def receive(self) -> Generator[Task]:
+        while self.queue:
+            yield self.queue.pop()
 
 
 class RedisWorkQueue:
     def __init__(self, url: str):
+        pass
+
+    def send(self, task: Task):
+        pass
+
+    def receive(self) -> Generator[Task]:
         pass

82640efc8f116039b814aecde0e1aa3d9a683d73
Nick Pilon <npilon@lexmachina.com>
2021-06-02 15:50:42 -0700

    Add until_exhausted test for redis too

--- tests/functional/test_argument_dispatching.py
+++ tests/functional/test_argument_dispatching.py
@@ -4,6 +4,9 @@ from hank import (
     LocalMemoryWorkQueue,
     LocalMemoryResultStore,
     LocalMemoryWorkSite,
+    RedisWorkQueue,
+    RedisResultStore,
+    RedisWorkSite,
 )
 
 # argument_unpacking_plan includes setting result from return value.
@@ -26,3 +29,20 @@ def test_argument_dispatching():
     work_site = LocalMemoryWorkSite(dispatcher, None)
     work_site.dispatch_until_exhausted()
     assert result.wait() == 5
+
+
+def test_argument_dispatching_redis():
+    dispatcher = Dispatcher()
+
+    # Unnamed queue - default for all messages.
+    dispatcher.add_queue(
+        RedisWorkQueue(url="redis://localhost:6379/0", queue="example_queue")
+    )
+    # Unnamed result store - default for any results.
+    dispatcher.add_result_store(RedisResultStore("redis://localhost:6379/1"))
+    dispatcher.add_plan(do_arithmetic)
+    # Store result could also take the name of a configured result store.
+    result = dispatcher.send(do_arithmetic.task(4, 3).options(store_result=True))
+    work_site = RedisWorkSite(dispatcher, None)
+    work_site.dispatch_until_exhausted()
+    assert result.wait() == 7
